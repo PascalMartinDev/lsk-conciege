0986de5ce72c3c504764b445f7f28682876aadc1
Pascal Martin <connect@pascalmartin.dev>
2025-09-06 13:12:12 +1000

    Added Tailwindcss plus starter files and assets and AppContent component

---
+++ src/components/core/AppContent.tsx
@@ -0,0 +1,9 @@
+const AppContent = () => {
+  return (
+  <div className="flex flex-col items-center justify-center min-h-screen">
+    <h1 className="text-center">Lightspeed Conciege</h1>
+    <h4 className="text-center">Started project</h4>
+  </div>)
+}
+
+export default AppContent;
