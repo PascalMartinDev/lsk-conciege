4c20978cc4a21baf65ba49739703675e01a1df9a
Pascal Martin <connect@pascalmartin.dev>
2025-09-06 13:24:38 +1000

    Added Footer component

---
+++ src/components/core/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-600">
+      <div className="container mx-auto text-left">
+        <div className="text-center">
+          <p className="text-center py-4 text-white">
+            Developed by Pascal Martin<span className="mx-3">|</span>For support
+            email: connect@pascalmartin.dev
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
